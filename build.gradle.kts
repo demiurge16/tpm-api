be916a616a5ed560481dbf1c10bf15b3a571478c
Oleh Shatskyi <olegshatskyj@gmail.com>
2023-09-09 23:59:36 +0200

    Optimize gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,57 +23,39 @@ repositories {
     mavenCentral()
 }
 
-val springCloudVersion: String by extra {
-    "2022.0.4"
-}
-
-val liquibaseVersion: String by extra {
-    "4.20.0"
-}
-
-val springdocVersion: String by extra {
-    "2.1.0"
-}
-
-val minioVersion: String by extra {
-    "8.5.2"
-}
-
-val picocliVersion: String by extra {
-    "4.6.3"
-}
+val springCloudVersion: String by extra { "2022.0.4" }
+val liquibaseVersion: String by extra { "4.20.0" }
+val springdocVersion: String by extra { "2.1.0" }
+val minioVersion: String by extra { "8.5.2" }
+val picocliVersion: String by extra { "4.6.3" }
+val keycloakVersion: String by extra { "22.0.1" }
+val openCsvVersion: String by extra { "5.8" }
+val logstashLogbackEncoderVersion: String by extra { "7.4" }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-quartz")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.keycloak:keycloak-core:22.0.1")
-    implementation("org.keycloak:keycloak-policy-enforcer:22.0.1")
-    implementation("org.keycloak:keycloak-admin-client:22.0.1")
-    implementation("org.keycloak:keycloak-authz-client:22.0.1")
+    implementation("org.keycloak:keycloak-core:$keycloakVersion")
+    implementation("org.keycloak:keycloak-policy-enforcer:$keycloakVersion")
+    implementation("org.keycloak:keycloak-admin-client:$keycloakVersion")
+    implementation("org.keycloak:keycloak-authz-client:$keycloakVersion")
     implementation("org.liquibase:liquibase-core:$liquibaseVersion")
     implementation("org.liquibase.ext:liquibase-hibernate6:$liquibaseVersion")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-    implementation("com.opencsv:opencsv:5.8")
+    implementation("com.opencsv:opencsv:$openCsvVersion")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
     implementation("io.minio:minio:$minioVersion")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("org.postgresql:postgresql")
