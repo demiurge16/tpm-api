cede8445b76acaa8395f0014cd7cf9e3b5263567
Oleh Shatskyi <olegshatskyj@gmail.com>
2024-02-20 01:31:27 +0100

    Add export for time entries

--- src/main/kotlin/net/nuclearprometheus/tpm/applicationserver/adapters/controllers/task/TaskTimeEntryController.kt
+++ src/main/kotlin/net/nuclearprometheus/tpm/applicationserver/adapters/controllers/task/TaskTimeEntryController.kt
@@ -1,11 +1,15 @@
 package net.nuclearprometheus.tpm.applicationserver.adapters.controllers.task
 
+import com.opencsv.bean.StatefulBeanToCsvBuilder
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.TaskTimeEntryApplicationService
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.requests.CreateTimeEntry
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.requests.ListTimeEntries
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.responses.TimeEntry
 import net.nuclearprometheus.tpm.applicationserver.config.logging.loggerFor
 import net.nuclearprometheus.tpm.applicationserver.domain.queries.pagination.Page
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,7 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.OutputStreamWriter
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import java.time.LocalDate
 import java.util.UUID
+import kotlin.concurrent.thread
 
 @RestController
 @RequestMapping("/api/v1/task/{taskId}/time-entry")
@@ -25,6 +34,41 @@ class TaskTimeEntryController(private val service: TaskTimeEntryApplicationServi
     fun getTimeEntries(@PathVariable(name = "taskId") taskId: UUID, query: ListTimeEntries): ResponseEntity<Page<TimeEntry>> {
         logger.info("GET /api/v1/task/$taskId/time-entry")
         return ResponseEntity.ok().body(service.getTimeEntriesForTask(taskId, query))
+    }
+
+    @GetMapping("/export", produces = ["text/csv"])
+    fun export(@PathVariable(name = "taskId") taskId: UUID, query: ListTimeEntries) = with(logger) {
+        info("GET /api/v1/project/export")
+
+        val files = service.getTimeEntriesForTask(taskId, query)
+
+        val output = PipedOutputStream()
+        val input = PipedInputStream(output)
+
+        thread {
+            val writer = OutputStreamWriter(output)
+            val beanToCsv = StatefulBeanToCsvBuilder<TimeEntry>(writer).build()
+            beanToCsv.write(files.items)
+            writer.flush()
+            writer.close()
+            output.close()
+        }
+
+        val name = "tasks-${LocalDate.now()}.csv"
+        val resource = InputStreamResource(input)
+
+        ResponseEntity.ok()
+            .headers(
+                HttpHeaders().apply {
+                    add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$name")
+                    add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+                    add(HttpHeaders.PRAGMA, "no-cache")
+                    add(HttpHeaders.EXPIRES, "0")
+                }
+            )
+
+            .contentType(MediaType.parseMediaType("text/csv"))
+            .body(resource)
     }
 
     @PostMapping("")
--- src/main/kotlin/net/nuclearprometheus/tpm/applicationserver/adapters/controllers/task/TimeEntryController.kt
+++ src/main/kotlin/net/nuclearprometheus/tpm/applicationserver/adapters/controllers/task/TimeEntryController.kt
@@ -1,15 +1,26 @@
 package net.nuclearprometheus.tpm.applicationserver.adapters.controllers.task
 
+import com.opencsv.bean.StatefulBeanToCsvBuilder
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.TimeEntryApplicationService
+import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.requests.ListTasks
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.requests.ListTimeEntries
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.requests.UpdateTimeEntry
+import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.responses.Task
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.responses.TimeEntry
 import net.nuclearprometheus.tpm.applicationserver.adapters.applicationservices.task.responses.TimeEntryStatusResponse
 import net.nuclearprometheus.tpm.applicationserver.config.logging.loggerFor
 import net.nuclearprometheus.tpm.applicationserver.domain.queries.pagination.Page
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.io.OutputStreamWriter
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import java.time.LocalDate
 import java.util.*
+import kotlin.concurrent.thread
 
 @RestController
 @RequestMapping("/api/v1/time-entry")
@@ -21,6 +32,41 @@ class TimeEntryController(private val service: TimeEntryApplicationService) {
     fun getTimeEntries(query: ListTimeEntries): ResponseEntity<Page<TimeEntry>> {
         logger.info("GET /api/v1/time-entry")
         return ResponseEntity.ok().body(service.getTimeEntries(query))
+    }
+
+    @GetMapping("/export", produces = ["text/csv"])
+    fun export(query: ListTimeEntries) = with(logger) {
+        info("GET /api/v1/project/export")
+
+        val files = service.getTimeEntries(query)
+
+        val output = PipedOutputStream()
+        val input = PipedInputStream(output)
+
+        thread {
+            val writer = OutputStreamWriter(output)
+            val beanToCsv = StatefulBeanToCsvBuilder<TimeEntry>(writer).build()
+            beanToCsv.write(files.items)
+            writer.flush()
+            writer.close()
+            output.close()
+        }
+
+        val name = "tasks-${LocalDate.now()}.csv"
+        val resource = InputStreamResource(input)
+
+        ResponseEntity.ok()
+            .headers(
+                HttpHeaders().apply {
+                    add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$name")
+                    add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
+                    add(HttpHeaders.PRAGMA, "no-cache")
+                    add(HttpHeaders.EXPIRES, "0")
+                }
+            )
+
+            .contentType(MediaType.parseMediaType("text/csv"))
+            .body(resource)
     }
 
     @GetMapping("/{timeEntryId}")
